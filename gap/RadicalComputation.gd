faa4bf2b7a3dfbe52178277b803ac5e31ca49796
Eva Maria Hemmerling <evamaria@hemmerling-nw.de>
2013-11-10 09:02:09 +0100

    Missed these lines in the last commit: Creating RadicalComputation.gd/gi files

---
+++ gap/RadicalComputation.gd
@@ -0,0 +1,20 @@
+#############################################################################
+##
+##  RadicalComputation.gd                                    PrimaryDecomposition package
+##
+##  Copyright 2013,      Mohamed Barakat, University of Kaiserslautern
+##                  Eva Maria Hemmerling, University of Kaiserslautern
+##
+##  Declaration of some functions for radical computation.
+##
+#############################################################################
+
+#! @Chapter RadicalComputation
+
+####################################
+#
+#! @Section Attributes
+#
+####################################
+
+
---
+++ gap/RadicalComputation.gi
@@ -0,0 +1,18 @@
+#############################################################################
+##
+##  RadicalComputation.gi                                    PrimaryDecomposition package
+##
+##  Copyright 2013,      Mohamed Barakat, University of Kaiserslautern
+##                  Eva Maria Hemmerling, University of Kaiserslautern
+##
+##  Implementation of some functions for radical computation.
+##
+#############################################################################
+
+####################################
+#
+# methods for attributes:
+#
+####################################
+
+
